f3df5251cb5f1433ac0dd2a2a3429aa514414472
Schott Taylor <schotttaylor@gmail.com>
2023-08-29 03:24:24 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { StyledEngineProvider } from '@mui/material';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <div className="App">
+        <Layout />
+      </div>
+    </StyledEngineProvider>
   );
 }
 
